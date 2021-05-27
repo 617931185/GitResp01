496bac564b0b36815058658d435a495881dd909b
mist <617931185@qq.com>
2021-05-27 20:16:01 +0800

    add keyListener

--- src/main/java/com/mist/tank/Main.java
+++ src/main/java/com/mist/tank/Main.java
@@ -1,6 +1,6 @@
 package com.mist.tank;
 
-public class T {
+public class Main {
     public static void main(String[] args) {
         TankFrame tf = new TankFrame();
     }
--- src/main/java/com/mist/tank/TankFrame.java
+++ src/main/java/com/mist/tank/TankFrame.java
@@ -1,15 +1,28 @@
 package com.mist.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
+    int x=200,y=200;
     public TankFrame(){
         this.setSize(800,600);
         this.setResizable(false);
         this.setTitle("tank war");
         this.setVisible(true);
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println("key pressed");
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+                System.out.println("key released");
+            }
+        });
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -20,6 +33,8 @@ public class TankFrame extends Frame {
     //传入一支画笔，绘制窗口，每次需要重新绘制就会被调用
     @Override
     public void paint(Graphics g){
-        g.fillRect(200,200,50,50);
+        g.fillRect(x,y,50,50);
+        x+=10;
+        y+=10;
     }
 }
